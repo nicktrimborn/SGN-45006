bf076f8122a37b06998b3545d46673497c9b6d77
Nicolas Trimborn <nicolas.trimborn@student.tut.fi>
2019-04-10 19:04:03 +0300

    completed ex

--- exercise6/Matlab/camcalibDLT.m
+++ exercise6/Matlab/camcalibDLT.m
@@ -5,18 +5,30 @@ function P=camcalibDLT(Xworld,Xim)
     
     % Create the matrix A 
     %%-your-code-starts-here-%%
-
+    n = 8;
+    A = zeros(16,12);
+    y = Xim(:,2);
+    x = Xim(:,1);
+    for i=1:8 
+        A(i*2-1,:) = [zeros(1,4) Xworld(i,:) -y(i)*Xworld(i,:)];
+        A(i*2,:) = [Xworld(i,:) zeros(1,4) -x(i)*Xworld(i,:)];
+    end
+    disp(A)
     %%-your-code-ends-here-%%
+       
     
     % Perform homogeneous least squares fitting,
     % the best solution is given by the eigenvector of 
     % A.T*A with the smallest eigenvalue.
     %%-your-code-starts-here-%%
-
+    evs=A'*A;
+    [V,D]= eig(evs)
+    [d,ind]= min(diag(D));
+    ev= V(:,ind);
     %%-your-code-ends-here-%%
 
     % Reshape the eigenvector into a projection matrix P
-    %P=(reshape(ev,4,3))'; % uncomment this
-    P = [1 0 0 0;0 1 0 0;0 0 1 0];  % remove this
+    P =(reshape(ev,4,3))'; % uncomment this
+    %P = [1 0 0 0;0 1 0 0;0 0 1 0];  % remove this
 
 end
--- exercise6/Matlab/trianglin.m
+++ exercise6/Matlab/trianglin.m
@@ -7,7 +7,21 @@ function X=trianglin(P1,P2,x1,x2)
     % Form A and get the least squares solution from the eigenvector 
     % corresponding to the smallest eigenvalue
     %%-your-code-starts-here-%%
-    X = ones(4,1);  % replace with your implementation
+    x1x = [0 -x1(3) x1(2);...
+           x1(3) 0 -x1(1);...
+           -x1(2) x1(1) 0]; 
+    x2x = [0 -x2(3) x2(2);...
+           x2(3) 0 -x2(1);...
+           -x2(2) x2(1) 0];
+     
+    A= [x1x*P1; x2x*P2];    
+    
+    evs=A'*A;
+    [V,D]= eig(evs)
+    [d,ind]= min(diag(D));
+    X= V(:,ind);
+        
+    %X = ones(4,1);  % replace with your implementation
     %%-your-code-ends-here-%%
     
 
